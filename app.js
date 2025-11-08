d1ca7963e7a8105f96764252b8d35ce28e3819df
Pedro Cabral <77162808+quebrale@users.noreply.github.com>
2025-11-08 15:35:53 +0000

    Create CRUD functions for Class Aluno using service layer instead of class methods

--- app.js
+++ app.js
@@ -5,8 +5,21 @@ import { Aula } from './classes/Aula.js';
 import { Pagamento } from './classes/Pagamento.js';
 import { Professor } from './classes/Professor.js';
 
+const novoAluno = new Aluno()
+const aluno = novoAluno.createAluno(gerarDadosAluno(data));
+novoAluno.readAluno(aluno.id)
 
-const novoAluno = new Aluno ({...gerarDadosAluno(data)});
+console.log("Aluno antes da alteracao:", novoAluno);
+
+novoAluno.updateAluno(aluno.id,{nome:'Joana',apelido:'Baixaria'})
+
+console.log("Aluno depois da alteracao:",novoAluno.updateAluno(aluno.id,{nome:'Joana',apelido:'Baixaria'}));
+
+novoAluno.deleteAluno(aluno.id)
+
+console.log ("Deleted:",novoAluno)
+
+/*const novoAluno = new Aluno ({...gerarDadosAluno(data)});
 const novoProfessor = new Professor ({...gerarDadosProfessor(data)});
 const novoAula = new Aula ({...gerarDadosAula(data)});
 const novoPagamento = new Pagamento ({...gerarDadosPagamento(data)});
@@ -14,4 +27,4 @@ const novoPagamento = new Pagamento ({...gerarDadosPagamento(data)});
 console.log(novoAluno);
 console.log(novoProfessor);
 console.log(novoAula);
-console.log(novoPagamento);
+console.log(novoPagamento);*/
