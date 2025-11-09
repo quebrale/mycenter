a262bfb1958d8afca9007a60a578778cc002c14b
Pedro Cabral <77162808+quebrale@users.noreply.github.com>
2025-11-09 17:05:58 +0000

    Create CRUD function for the Aula class

--- app.js
+++ app.js
@@ -5,7 +5,23 @@ import { Aula } from './classes/Aula.js';
 import { Pagamento } from './classes/Pagamento.js';
 import { Professor } from './classes/Professor.js';
 
+const newAula = new Aula();
+const aula = newAula.createAula(gerarDadosAula(data));
+const lerAula = newAula.readAula(aula.aula_id);
+console.log(lerAula);
+const atualizarAula = newAula.updateAula(aula.aula_id,{disciplina: 'Matematica', tipo_aula:'Individual (Presencial)'});
+console.log("dados atualizados: ", atualizarAula);
+newAula.deleteAula(aula.aula_id);
+console.log(newAula);
+
+
+
+
+/*
+// TESTING NEW ALUNO SERVICE LAYER 
+
 const novoAluno = new Aluno()
+
 const aluno = novoAluno.createAluno(gerarDadosAluno(data));
 novoAluno.readAluno(aluno.id)
 
@@ -18,8 +34,10 @@ console.log("Aluno depois da alteracao:",novoAluno.updateAluno(aluno.id,{nome:'J
 novoAluno.deleteAluno(aluno.id)
 
 console.log ("Deleted:",novoAluno)
+*/
 
-/*const novoAluno = new Aluno ({...gerarDadosAluno(data)});
+/*
+const novoAluno = new Aluno ({...gerarDadosAluno(data)});
 const novoProfessor = new Professor ({...gerarDadosProfessor(data)});
 const novoAula = new Aula ({...gerarDadosAula(data)});
 const novoPagamento = new Pagamento ({...gerarDadosPagamento(data)});
